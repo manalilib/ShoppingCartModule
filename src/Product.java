23d61b7f53f4d03b29bdbef738ef60f663da1c08
Benjar Manalili <manalilib@localhost.localdomain>
2017-01-09 10:04:51 +0800

    Initial commit: Added Main Class and Product Class.

---
+++ src/Product.java
@@ -0,0 +1,14 @@
+/**
+ * Created by manalilib on 1/9/17.
+ */
+public class Product {
+    String code;
+    String name;
+    double price;
+
+    public Product(String code, String name, double price) {
+        this.code = code;
+        this.name = name;
+        this.price = price;
+    }
+}
---
+++ src/ShoppingCart.java
@@ -0,0 +1,10 @@
+/**
+ * Created by manalilib on 1/9/17.
+ */
+import java.util.*;
+
+public class ShoppingCart {
+public static void  main(String[] args){
+
+}
+}
