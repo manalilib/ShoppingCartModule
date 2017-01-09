3d8b2c33a9e2743d918c227884b79b9c9fe1223a
Benjar Manalili <manalilib@localhost.localdomain>
2017-01-09 13:39:46 +0800

    SCM: Initialize products/vars.

---
+++ src/PromoFreebies.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+/**
+ * Created by manalilib on 1/9/17.
+ */
+public class PromoFreebies {
+    ArrayList<Product> totalFreebies;
+    double totalDeduction;
+
+    public PromoFreebies(ArrayList<Product> totalFreebies, double totalDeduction) {
+        this.totalFreebies = totalFreebies;
+        this.totalDeduction = totalDeduction;
+    }
+}
--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -5,6 +5,11 @@ import java.util.*;
 
 public class ShoppingCart {
 public static void  main(String[] args){
+    HashMap<String, Product> products = new HashMap<String, Product>();
+    products.put("ult_small", new Product("ult_small", "Unlimited 1GB", 24.90));
+    products.put("ult_medium", new Product("ult_medium", "Unlimited 2GB", 29.90));
+    products.put("ult_large", new Product("ult_large", "Unlimited 5GB", 44.90));
+    products.put("1gb", new Product("1gb", "1 GB Data-pack", 9.90));
 
 }
 }
